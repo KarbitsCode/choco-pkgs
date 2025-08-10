11a9d9490157de2fc6ca0d738cd4134f89e2dc1c
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-08-10 09:58:47 +0800

    Add crystalmarkretro version 1.0.2

---
+++ crystalmarkretro.install/1.0.2/tools/chocolateyuninstall.ps1
@@ -0,0 +1,38 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# $name: CrystalMark Retro (Install) -> CrystalMark Retro
+$name = $($env:ChocolateyPackageTitle -replace '\s*\(.*?\)', '')
+# $fileName: CrystalMark Retro -> CrystalMarkRetro
+$fileName = $($name -replace '\s+', '')
+$version = '1.0.2'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = "$name*"
+  fileType      = 'EXE'
+  silentArgs    = '/SILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)"
+
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
