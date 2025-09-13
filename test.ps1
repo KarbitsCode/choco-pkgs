5561ca20c8c7501d9517f43049b045c32ca05662
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-09-13 14:49:02 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -44,7 +44,8 @@ function Download-Installer {
 	}
 
 	$checksumType = ($content | Select-String -Pattern '^  checksum_type: (.*)$').Matches.Groups[1].Value
-	$expectedChecksum = ($content | Select-String -Pattern '^  file_checksum: (.*)$').Matches.Groups[1].Value
+	$expectedChecksum = ($content | Select-String -Pattern '^  file_checksum: (.*)$').Matches.Groups[1].Value.ToUpper()
+	$actualChecksum = $(Get-RemoteChecksum $url $checksumType).ToUpper()
 
 	$toolsDir = Join-Path $PackageDir "tools"
 	if (-not (Test-Path $toolsDir)) {
@@ -62,7 +63,7 @@ function Download-Installer {
 		Write-Output "Expected checksum: $expectedChecksum"
 		Write-Output "Actual checksum:   $actualChecksum"
 
-		if ($actualChecksum -ne $expectedChecksum.ToUpper()) {
+		if ($actualChecksum -ne $expectedChecksum) {
 			throw "Checksum verification failed for $outFile"
 		} else {
 			Write-Color "Checksum verification passed for $outFile" -Foreground Green
@@ -74,50 +75,63 @@ function Download-Installer {
 
 # =================================================================================================
 
-$pkgFolder = "."
-Remove-Item *.nupkg
-Get-ChildItem -Path "." -Recurse | Where-Object { $_.Extension -in ".zip", ".exe" } | Remove-Item
+function Validate-And-Prepare-Package {
+	$pkgFolder = "."
+	Remove-Item *.nupkg
+	Get-ChildItem -Path $pkgFolder -Recurse | Where-Object { $_.Extension -in ".zip", ".exe" } | Remove-Item
 
-Get-ChildItem -Path $pkgFolder -Recurse -Filter *.nuspec | ForEach-Object {
-	$dir = $_.DirectoryName
-	$file = $_.FullName
+	Get-ChildItem -Path $pkgFolder -Recurse -Filter *.nuspec | ForEach-Object {
+		$dir = $_.DirectoryName
+		$file = $_.FullName
 
-	cnc $dir
-	Download-Installer $dir
-	choco pack $file
+		cnc $dir
+		Download-Installer $dir
+		choco pack $file
+	}
 }
 
 # =================================================================================================
 
-Write-Color "Getting list of packages before install test..." -Foreground Blue
-$installedBefore = choco list --limit-output | ForEach-Object { ($_ -split '\|')[0] }
+function Test-Install-Package {
+	Write-Color "Getting list of packages before install test..." -Foreground Blue
+	$installedBefore = choco list --limit-output | ForEach-Object { ($_ -split '\|')[0] }
 
-Get-ChildItem -Path $pkgFolder -Filter *.nupkg | ForEach-Object {
-	$filename = [System.IO.Path]::GetFileNameWithoutExtension($_.FullName)
+	Get-ChildItem -Path $pkgFolder -Filter *.nupkg | ForEach-Object {
+		$filename = [System.IO.Path]::GetFileNameWithoutExtension($_.FullName)
 
-	# Getting the pkgName and pkgVersion directly from the filename
-	if ($filename -match '^(?<id>.+)\.(?<version>\d+\.\d+\.\d+(-[A-Za-z0-9]+)?)$') {
-		$pkgName = $matches['id']
-		$pkgVersion = $matches['version']
+		# Getting the pkgName and pkgVersion directly from the filename
+		if ($filename -match '^(?<id>.+)\.(?<version>\d+\.\d+\.\d+(-[A-Za-z0-9]+)?)$') {
+			$pkgName = $matches['id']
+			$pkgVersion = $matches['version']
+		}
+
+		# Skipping .install package since it probably already installed by its metapackage
+		if ($pkgName -notlike "*.install") {
+			Write-Color "Installing $pkgName version $pkgVersion..." -Foreground Blue
+			choco install $pkgName --version=$pkgVersion --source=$pkgFolder --yes --force
+		}
 	}
 
-	# Skipping .install package since it probably already installed by its metapackage
-	if ($pkgName -notlike "*.install") {
-		Write-Color "Installing $pkgName version $pkgVersion..." -Foreground Blue
-		choco install $pkgName --version=$pkgVersion --source=$pkgFolder --yes --force
+	Write-Color "Getting list of packages after install test..." -Foreground Blue
+	$installedAfter = choco list --limit-output | ForEach-Object { ($_ -split '\|')[0] }
+	$newlyInstalled = $installedAfter | Where-Object { $_ -notin $installedBefore }
+
+	if ($newlyInstalled.Length -ne 0) {
+		Write-Color "Uninstalling newly installed packages: $($newlyInstalled -join ', ')" -Foreground Blue
+		choco uninstall @newlyInstalled -yes
+	} else {
+		Write-Warning "There is no change in list of packages, it's possible if all the previous package installs failed."
 	}
+
+	Get-ChildItem -Path $pkgFolder -Recurse | Where-Object { $_.Extension -in ".zip", ".exe" } | Remove-Item
+	Remove-Item *.nupkg
 }
 
-Write-Color "Getting list of packages after install test..." -Foreground Blue
-$installedAfter = choco list --limit-output | ForEach-Object { ($_ -split '\|')[0] }
-$newlyInstalled = $installedAfter | Where-Object { $_ -notin $installedBefore }
-
-if ($newlyInstalled.Length -ne 0) {
-	Write-Color "Uninstalling newly installed packages: $($newlyInstalled -join ', ')" -Foreground Blue
-	choco uninstall @newlyInstalled -yes
-} else {
-	Write-Warning "There is no change in list of packages, it's possible if all the previous package installs failed."
+function Main {
+	Validate-And-Prepare-Package
+	Test-Install-Package
 }
 
-Get-ChildItem -Path "." -Recurse | Where-Object { $_.Extension -in ".zip", ".exe" } | Remove-Item
-Remove-Item *.nupkg
+if ((Split-Path -Path $MyInvocation.InvocationName -Leaf) -eq $MyInvocation.MyCommand.Name) {
+    Main
+}
