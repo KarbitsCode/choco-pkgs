fa81db94a8bd43a9e88a62bd1879630fba146064
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-09-27 09:33:31 +0800

    Add crystalmarkretro version 2.0.5

---
+++ crystalmarkretro.portable/2.0.5/tools/chocolateyinstall.ps1
@@ -0,0 +1,48 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# $name: CrystalMark Retro (Install) -> CrystalMark Retro
+$name = $($env:ChocolateyPackageTitle -replace '\s*\(.*?\)', '')
+# $fileName: CrystalMark Retro -> CrystalMarkRetro
+$fileName = $($name -replace '\s+', '')
+$version = '2.0.5'
+$fileLocation = Join-Path $toolsDir "$($fileName)$($version -replace '\.', '_').zip"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  file          = $fileLocation
+  softwareName  = "$name*"
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Get-ChildItem "$toolsDir\*.zip" -Recurse:$false | ForEach-Object {
+  Remove-Item $_ -ErrorAction SilentlyContinue
+  if (Test-Path $_) {
+    Write-Debug "Failed to delete: $($_.FullName)"
+    Set-Content "$($_.FullName).ignore" -Value '' -Force
+  }
+}
+
+# For executable shim
+$osArch = (Get-CimInstance Win32_OperatingSystem | Select-Object -ExpandProperty OSArchitecture)
+if ($osArch -eq "ARM 64-bit") {
+  $exePath = "$($fileName)A64.exe"
+} elseif ($osArch -eq "64-bit") {
+  $exePath = "$($fileName)64.exe"
+} else {
+  $exePath = "$($fileName)32.exe"
+}
+
+Install-BinFile -Name $fileName -Path (Join-Path -Path $toolsDir -ChildPath $exePath)
+
+# Prevent unnecessary recursive shims
+Get-ChildItem -Path "$toolsDir\Resource" -Recurse -Filter *.exe | ForEach-Object {
+  $ignoreFile = "$($_.FullName).ignore"
+  if (-not (Test-Path $ignoreFile)) {
+    New-Item -Path $ignoreFile -ItemType File -Force | ForEach-Object {
+      Write-Debug "Adding ignore file: $($_.FullName)"
+    }
+  }
+}
