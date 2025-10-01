45a952dc8da0e273ba45badaf0f0442249099223
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-10-01 16:59:34 +0800

    Add crystalmarkretro version 2.0.6

---
+++ crystalmarkretro.install/2.0.6/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# $name: CrystalMark Retro (Install) -> CrystalMark Retro
+$name = $($env:ChocolateyPackageTitle -replace '\s*\(.*?\)', '')
+# $fileName: CrystalMark Retro -> CrystalMarkRetro
+$fileName = $($name -replace '\s+', '')
+$version = '2.0.6'
+$fileLocation = Join-Path $toolsDir "$($fileName)$($version -replace '\.', '_').exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  file          = $fileLocation
+  softwareName  = "$name*"
+  silentArgs    = '/SILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem "$toolsDir\*.exe" -Recurse:$false | ForEach-Object {
+  Remove-Item $_ -ErrorAction SilentlyContinue
+  if (Test-Path $_) {
+    Write-Debug "Failed to delete: $($_.FullName)"
+    Set-Content "$($_.FullName).ignore" -Value '' -Force
+  }
+}
---
+++ crystalmarkretro.portable/2.0.6/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# $name: CrystalMark Retro (Install) -> CrystalMark Retro
+$name = $($env:ChocolateyPackageTitle -replace '\s*\(.*?\)', '')
+# $fileName: CrystalMark Retro -> CrystalMarkRetro
+$fileName = $($name -replace '\s+', '')
+$version = '2.0.6'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = "$name*"
+  zipFileName   = "${env:ChocolateyPackageName}Install.zip"
+  name          = $fileName
+}
+
+Uninstall-ChocolateyZipPackage @packageArgs
+Uninstall-BinFile @packageArgs
