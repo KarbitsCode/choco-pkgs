fd479aae9a4e48d9800415e13265f7d1dc299710
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-01-06 15:21:48 +0800

    Add msiwrapper version 3.0.25.0

---
+++ msiwrapper.install/3.0.25.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = $toolsDir
+  fileType      = 'MSI'
+  url           = 'https://www.exemsi.com/downloads/msi_wrapper/MSI_Wrapper_3_0_25_0.msi'
+  softwareName  = 'MSI Wrapper*'
+  checksum      = '6007ADECC1CFE957CC6A52ED7A84D6238E4C30E350B361A707AD4AD46A15FF82'
+  checksumType  = 'sha256'
+  silentArgs    = "/qb! /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641, 1603)
+}
+
+Install-ChocolateyPackage @packageArgs
