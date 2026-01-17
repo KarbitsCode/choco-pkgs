741f8ba7e140f9674ba368e32f99faf762e5dd95
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-01-17 12:32:48 +0800

    Add msiwrapper version 3.0.31.0

---
+++ msiwrapper.install/3.0.31.0/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# Kill the app process before upgrade/uninstall
+@("MsiWrapperBatch", "MsiWrapper", "MsiSql") | ForEach-Object {
+  $name = $_
+  try {
+    Get-Process -Name $name -ErrorAction Stop | ForEach-Object {
+      try {
+        Write-Host "Try to stop running processes..."
+        Stop-Process -Id $_.Id -Force -ErrorAction Stop
+        Write-Host "Stopped process $($_.ProcessName).exe (PID: $($_.Id))"
+      } catch {
+        Write-Warning "Failed to stop ${name}: $_"
+      }
+    }
+  } catch {
+    Write-Debug "Did not find ${name}.exe process"
+  }
+}
---
+++ msiwrapper.install/3.0.31.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = $toolsDir
+  fileType      = 'MSI'
+  url           = 'https://www.exemsi.com/downloads/msi_wrapper/MSI_Wrapper_3_0_31_0.msi'
+  softwareName  = 'MSI Wrapper*'
+  checksum      = 'B112F28BAA9E0AC78553EF11C228F896E95EB4BC2F932E529DFD3E752A0203E4'
+  checksumType  = 'sha256'
+  silentArgs    = "/qb! /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641, 1603)
+}
+
+Install-ChocolateyPackage @packageArgs
