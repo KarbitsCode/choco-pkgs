015da11abda3005613e1c53bb846c92fbaed1235
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-02-03 15:35:11 +0800

    Add msiwrapper version 8.0.24.0

---
+++ msiwrapper.install/8.0.24.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  destination   = $toolsDir
+  fileType      = 'MSI'
+  url           = 'https://www.exemsi.com/downloads/msi_wrapper/MSI_Wrapper_8_0_24_0.msi'
+  softwareName  = 'MSI Wrapper*'
+  checksum      = 'E76638143D9619AEFAFDD54D906F051AB9F0F65285A419C806D966DCD37B674D'
+  checksumType  = 'sha256'
+  silentArgs    = "/qb! /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641, 1603)
+}
+
+Install-ChocolateyPackage @packageArgs
