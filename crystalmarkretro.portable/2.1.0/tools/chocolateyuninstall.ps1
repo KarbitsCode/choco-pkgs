baa9e7b21964cce939920706889c03df6fc985de
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-03-29 14:25:52 +0800

    Add crystalmarkretro version 2.1.0

---
+++ crystalmarkretro.portable/2.1.0/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# $name: CrystalMark Retro (Portable) -> CrystalMark Retro
+$name = $($env:ChocolateyPackageTitle -replace '\s*\(.*?\)', '')
+# $fileName: CrystalMark Retro -> CrystalMarkRetro
+$fileName = $($name -replace '\s+', '')
+$version = '2.1.0'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = "$name*"
+  zipFileName   = "${env:ChocolateyPackageName}Install.zip"
+  name          = $fileName
+}
+
+Uninstall-ChocolateyZipPackage @packageArgs
+Uninstall-BinFile @packageArgs
